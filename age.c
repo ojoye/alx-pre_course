519b45b706e9b19854d98f3f3a549b43b2222ef7
ojoye <ojoyesamson@gmail.com>
2023-05-01 18:28:58 +0000

    created a new .c file

---
+++ age.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	char age;
+
+	age = 33;      /*this is my age*/
+	printf("You are %d years old.\n",age);
+	age = age + 5;
+	printf("In five years you'll be %d years old!\n",age);
+
+	return(0);
+}
