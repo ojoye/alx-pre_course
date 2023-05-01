8eaceef66c36d9b4dd45cd5cfcc59ef3ddf8f1b0
ojoye <ojoyesamson@gmail.com>
2023-05-01 18:37:00 +0000

    Added comments to the file
    I added multiple comments and also made it betty compliant

--- age.c
+++ age.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - This is the entry point
+ *
+ * Return: Always returns 0 on success
+ */
+
+int main(void)
 {
 	char age;
 
-	age = 33;      /*this is my age*/
-	printf("You are %d years old.\n",age);
+	age = 33;
+	printf("You are %d years old.\n", age);
 	age = age + 5;
-	printf("In five years you'll be %d years old!\n",age);
+	printf("In five years you'll be %d years old!\n", age);
 
-	return(0);
+	return (0);
 }
